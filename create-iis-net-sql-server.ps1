b1c6ddf05e1048f677279e22194a19f0b2c760ea
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-04-01 10:40:06 -0400

    First checkin

---
+++ create-iis-net-sql-server.ps1
@@ -0,0 +1,113 @@
+Set-ExecutionPolicy Bypass -Scope Process
+
+# To list all Windows Features: dism /online /Get-Features
+# Get-WindowsOptionalFeature -Online 
+# LIST All IIS FEATURES: 
+# Get-WindowsOptionalFeature -Online | where FeatureName -like 'IIS-*'
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
+
+Enable-WindowsOptionalFeature -online -FeatureName NetFx4Extended-ASPNET45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Security
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Performance
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-IIS6ManagementCompatibility
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Metabase
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-StaticContent
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# If you need classic ASP (not recommended)
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASP
+
+
+# The following optional components require 
+# Chocolatey OR Web Platform Installer to install
+
+
+# Install UrlRewrite Module for Extensionless Urls (optional)
+###  & "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" /install /Products:UrlRewrite2 /AcceptEULA /SuppressPostFinish
+#choco install urlrewrite -y
+    
+# Install WebDeploy for Deploying to IIS (optional)
+### & "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" /install /Products:WDeployNoSMO /AcceptEULA /SuppressPostFinish
+# choco install webdeploy -y
+
+# Disable Loopback Check on a Server - to get around no local Logins on Windows Server
+# New-ItemProperty HKLM:\System\CurrentControlSet\Control\Lsa -Name "DisableLoopbackCheck" -Value "1" -PropertyType dword
+
+#SQL Server
+function Install-SqlServer {
+  [CmdletBinding()]
+  param (
+    # The path to the SQL Server installation image
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $ImagePath,
+
+    # The extraction location for the installation media, used as the SourcePath for the DSC configuration
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $SourcePath
+  )
+
+  begin {
+    New-Item -Path $SourcePath -ItemType Directory
+    Copy-Item -Path (Join-Path -Path (Get-PSDrive -Name ((Mount-DiskImage -ImagePath $ImagePath -PassThru) | Get-Volume).DriveLetter).Root -ChildPath '*') -Destination $SourcePath -Recurse
+    Dismount-DiskImage -ImagePath $ImagePath
+
+    # Ensure NuGet package provider is present
+    Get-PackageProvider -Name NuGet -ForceBootstrap
+
+    # Ensure SqlServerDsc module is present
+    if (!(Get-Module SqlServerDsc)) {
+      Install-Module -Name SqlServerDsc -Force
+    }
+  }
+
+  process {
+    # Compile configuration
+    & .\sql-server-configuration.ps1 -SourcePath $SourcePath
+
+    # Run DSC configuration
+    Start-DscConfiguration -Path sql-server-configuration -Wait -Force -Verbose
+  }
+
+  end {
+    if (Test-DscConfiguration) {
+      Write-Host 'Removing configuration directory...'
+      Remove-Item .\SqlServerConfiguration -Recurse
+
+      Write-Host 'Removing extract directory...'
+      Remove-Item $SourcePath -Recurse
+
+      Write-Host 'Done.'
+    }
+  }
+}
+
